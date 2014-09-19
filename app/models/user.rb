2b3c2e68ad29af610d360b2c807f76e7e9df2be6
& <pair+jennmiller.michellechu77@devbootcamp.com>
2014-09-19 15:29:42 -0400

    Get followers & followed_users working

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :bites
   has_many :follower_relationships, foreign_key: :follower_id, class_name: "Relationship"
   has_many :followed_relationships, foreign_key: :followed_user_id, class_name: "Relationship"
-  has_many :followers, through: :relationships, source: :user, foreign_key: :follower_id
-  has_many :followed_users, through: :relationships, source: :user, foreign_key: :followed_user_id
+  has_many :followers, through: :follower_relationships, source: :user, foreign_key: :follower_id
+  has_many :followed_users, through: :followed_relationships, source: :user, foreign_key: :followed_user_id
   # Remember to create a migration!
 end
