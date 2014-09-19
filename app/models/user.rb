b350cc2f6f2fb8080e07a943eaff9d9335a0ceb0
& <pair+jennmiller.michellechu77@devbootcamp.com>
2014-09-19 15:09:56 -0400

    Fixed Relationship/User Associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :bites
-  has_many :relationships, foreign_key: :follower_id
-  has_many :relationships, foreign_key: :followed_user_id
+  has_many :follower_relationships, foreign_key: :follower_id, class_name: "Relationship"
+  has_many :followed_relationships, foreign_key: :followed_user_id, class_name: "Relationship"
   has_many :followers, through: :relationships, source: :user, foreign_key: :follower_id
   has_many :followed_users, through: :relationships, source: :user, foreign_key: :followed_user_id
   # Remember to create a migration!
